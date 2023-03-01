66719be67ed937276f588bab4df7366cf70d3f9d
Aneliya <97451473+aneliya-abdimalik@users.noreply.github.com>
2023-03-01 16:37:40 +0300

    Update Maze.php

--- Maze.php
+++ Maze.php
@@ -1,69 +1,60 @@
-function dijkstra($graph, $start, $destination) {
-    $distances = array_fill(0, count($graph), INF);
-    $visited = array_fill(0, count($graph), false);
-    $distances[$start] = 0;
 
-    for ($i = 0; $i < count($graph); $i++) {
-        $minDist = INF;
-        $current = -1;
-
-        // find unvisited node with the smallest distance
-        for ($j = 0; $j < count($graph); $j++) {
-            if (!$visited[$j] && $distances[$j] < $minDist) {
-                $minDist = $distances[$j];
-                $current = $j;
-            }
-        }
-
-        if ($current == -1) {
-            // no path found
-            return null;
-        }
-
-        $visited[$current] = true;
-
-        // update distances of neighboring nodes
-        for ($j = 0; $j < count($graph); $j++) {
-            if ($graph[$current][$j] > 0 && !$visited[$j]) {
-                $newDist = $distances[$current] + $graph[$current][$j];
-                if ($newDist < $distances[$j]) {
-                    $distances[$j] = $newDist;
-                }
-            }
-        }
-
-        if ($current == $destination) {
-            // shortest path found
-            return $distances[$destination];
-        }
+<?php
+error_reporting(0);
+echo "\n\n\nEnter row and column: \n";
+ 
+// Right trim fgets(user input)
+$dimention1 = rtrim(fgets($stdin));
+ 
+// Right trim fgets(user input)
+$dimention2 = rtrim(fgets($stdin));
+ 
+echo "Entered row and column: " .
+    $dimention1 . "x" . $dimention1 . " \n";
+ 
+$maze_arr = [];
+$k = 0;
+ 
+for ($row = 0; $row < $dimention1; $row++) {
+    for ($col = 0; $col < $dimention2; $col++) {
+        $maze_arr[$row][$col]= $k++;
     }
-
-    // no path found
-    return null;
 }
 
 
-$graph = array(
-    array(0, 4, 0, 0, 0, 0, 0, 8, 0),
-    array(4, 0, 8, 0, 0, 0, 0, 1, 0),
-    array(0, 8, 0, 7, 0, 4, 0, 0, 2),
-    array(0, 0, 7, 0, 9, 4, 0, 0, 0),
-    array(0, 0, 0, 9, 0, 5, 0, 0, 0),
-    array(0, 0, 4, 7, 3, 0, 2, 0, 0),
-    array(0, 0, 0, 0, 0, 2, 0, 1, 6),
-    array(8, 9, 0, 0, 0, 0, 1, 0, 7),
-    array(0, 0, 2, 0, 0, 0, 6, 7, 0)
-);
+//the start and the end
+$start_point = 1;
+$end_point = 5;
 
-$start = 0;
-$destination = 4;
+//initialize the array for storing
+$S = array();//the nearest path with its parent and weight
+$Q = array();//the left nodes without the nearest path
+foreach(array_keys($maze_arr) as $val) $Q[$val] = 99999;
+$Q[$start_point] = 0;
 
-$shortestPath = dijkstra($graph, $start, $destination);
-
-if ($shortestPath === null) {
-    echo "No path found.";
-} else {
-    echo "Shortest path from $start to $destination is $shortestPath.";
+//start calculating
+while(!empty($Q)){
+    $min = array_search(min($Q), $Q);//the most min weight
+    if($min == $end_point) break;
+    foreach($maze_arr[$min] as $key=>$val) if(!empty($Q[$key]) && $Q[$min] + $val < $Q[$key]) {
+        $Q[$key] = $Q[$min] + $val;
+        $S[$key] = array($min, $Q[$key]);
+    }
+    unset($Q[$min]);
 }
 
+//list the path
+$path = array();
+$pos = $end_point;
+while($pos != $start_point){
+    $path[] = $pos;
+    $pos = $S[$pos][0];
+}
+$path[] = $start_point;
+$path = array_reverse($path);
 
+//print result
+echo "<br />From $start_point to $end_point";
+echo "<br />The length is ".$S[$end_point][1];
+echo "<br />Path is ".implode('->', $path);
+?>
